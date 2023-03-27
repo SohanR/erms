445e88d6bee23a9092de56de2a904627722a878e
sohanR <sohanjs.bd@gmail.com>
2023-03-27 15:06:51 +0600

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import TeacherLogin from './auth/TeacherLogin'
 import Home from './components/Home'
 import Nav from './components/Nav'
-
+import CreateAccountForm from './forms/CreateAccountForm'
+import EmailVerificationForm from './forms/EmailVerificationForm'
 function App() {
   
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />    
         <Route path="/teacher/login" element={<TeacherLogin />} />
+        <Route path="/teacher/verify" element={<EmailVerificationForm />} />
+        <Route path="/teacher/create-account" element={<CreateAccountForm/>} />
       </Routes>
 
     </div>
